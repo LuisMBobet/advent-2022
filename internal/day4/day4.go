50c3c171b95f7705f1e660927e2303aa068a61cb
Luis <30813823+LuisMBobet@users.noreply.github.com>
2022-12-04 16:56:49 +0000

    Have a go with concurrency in day4

--- internal/day4/day4.go
+++ internal/day4/day4.go
@@ -4,6 +4,7 @@ import (
   "os"
   "strings"
   "strconv"
+  "sync"
 )
 
 func Part1(fileName string) int{
@@ -63,6 +64,52 @@ func Part2(fileName string) int{
       fullyContainedSections++
     }
   }
+
+  return fullyContainedSections
+}
+
+func Part2WithMutexConcurrency(fileName string) int{
+  dat,err := os.ReadFile(fileName)
+  if err != nil {
+    panic(err)
+  }
+
+  splitString := strings.Split(string(dat), "\n")
+  fullyContainedSections := 0
+
+
+  var wg sync.WaitGroup
+	var mtx sync.RWMutex
+
+  for _, line := range splitString[0:len(splitString)-1] {
+    wg.Add(1)
+    line := line
+
+    go func() {
+      var intPairs []int
+      pairs := strings.Split(strings.Replace(line, ",","-", 1), "-")
+
+      for _, pair := range pairs {
+        intPair, err := strconv.Atoi(pair)
+        if err != nil {
+          panic(err)
+        }
+        intPairs = append(intPairs, intPair)
+      }
+
+      if (intPairs[0] >= intPairs[2] && intPairs[0] <= intPairs[3]) ||
+      (intPairs[1] >= intPairs[2] && intPairs[1] <= intPairs[3]) ||
+      (intPairs[2] >= intPairs[0] && intPairs[2] <= intPairs[1]) ||
+      (intPairs[3] >= intPairs[0] && intPairs[3] <= intPairs[1]){
+        mtx.Lock()
+        fullyContainedSections++
+        mtx.Unlock()
+      }
+      wg.Done()
+    }()
+  }
+
+  wg.Wait()
 
   return fullyContainedSections
 }
--- internal/day4/day4_test.go
+++ internal/day4/day4_test.go
@@ -36,3 +36,17 @@ func TestDay4Part2(t *testing.T) {
     t.Errorf("Expected %d but received %d",expected, actual)
   }
 }
+
+func TestDay4Part2WithMutexConcurrency(t *testing.T) {
+  actual := day4.Part2("data.txt")
+  expected := 865
+  if actual != expected {
+    t.Errorf("Expected %d but received %d",expected, actual)
+  }
+}
+
+func BenchmarkTestDay4Part2(b *testing.B){
+  for n := 0; n < b.N; n++ {
+    day4.Part2("data.txt")
+  }
+}
