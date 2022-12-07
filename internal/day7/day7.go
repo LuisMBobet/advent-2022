a7970ef5cb4380ef193e8095b280f2b37aed909f
Luis <30813823+LuisMBobet@users.noreply.github.com>
2022-12-07 16:01:57 +0000

    Tidyup

--- internal/day7/day7.go
+++ internal/day7/day7.go
@@ -6,6 +6,38 @@ import (
   "strconv"
 )
 
+func readFileIntoLines(filePath string) []string {
+  dat,err := os.ReadFile(filePath)
+  if err != nil {
+    panic(err)
+  }
+
+  return strings.Split(string(dat), "\n")
+}
+
+func parseDirsStructure(commands []string) map[string]int {
+  dirs := make(map[string]int)
+  var currentDir []string
+
+  for _, line := range commands[0: len(commands)-1] {
+    splitLine := strings.Split(line, " ")
+    if splitLine[1] == "cd" && splitLine[2] != ".." {
+      currentDir = append(currentDir, splitLine[2])
+    } else if splitLine[1] == "cd" && splitLine[2] == ".." {
+      currentDir = currentDir[0:len(currentDir)-1]
+    } else {
+      intFirstValue, err := strconv.Atoi(splitLine[0])
+      if err == nil {
+        for index, _ := range currentDir {
+          fullFilePath := strings.Join(currentDir[0:index+1], "/")
+          dirs[fullFilePath] += intFirstValue
+        }
+      }
+    }
+  }
+  return dirs
+}
+
 func calcSumOfDirSizesLessThanCriteria (dirSizeMap map[string]int, criteria int) int {
   var sum int
   for _, element := range dirSizeMap {
@@ -27,61 +59,19 @@ func findSmallestDirLargerThanCriteria (dirSizeMap map[string]int, criteria int)
 }
 
 func Part1(fileName string) int{
-  dat,err := os.ReadFile(fileName)
-  if err != nil {
-    panic(err)
-  }
+  commands := readFileIntoLines(fileName)
 
-  splitString := strings.Split(string(dat), "\n")
+  dirs := parseDirsStructure(commands)
 
-  dirs := make(map[string]int)
-  var currentDir []string
-  for _, line := range splitString[0: len(splitString)-1] {
-    splitLine := strings.Split(line, " ")
-    if splitLine[1] == "cd" && splitLine[2] != ".." {
-      currentDir = append(currentDir, splitLine[2])
-    } else if splitLine[1] == "cd" && splitLine[2] == ".." {
-      currentDir = currentDir[0:len(currentDir)-1]
-    } else {
-      intFirstValue, err := strconv.Atoi(splitLine[0])
-      if err == nil {
-        for index, _ := range currentDir {
-          fullFilePath := strings.Join(currentDir[0:index+1], "/")
-          dirs[fullFilePath] += intFirstValue
-        }
-      }
-    }
-  }
   return calcSumOfDirSizesLessThanCriteria(dirs, 100000)
 }
 
 
 func Part2(fileName string) int{
-  dat,err := os.ReadFile(fileName)
-  if err != nil {
-    panic(err)
-  }
+  commands := readFileIntoLines(fileName)
 
-  splitString := strings.Split(string(dat), "\n")
+  dirs := parseDirsStructure(commands)
 
-  dirs := make(map[string]int)
-  var currentDir []string
-  for _, line := range splitString[0: len(splitString)-1] {
-    splitLine := strings.Split(line, " ")
-    if splitLine[1] == "cd" && splitLine[2] != ".." {
-      currentDir = append(currentDir, splitLine[2])
-    } else if splitLine[1] == "cd" && splitLine[2] == ".." {
-      currentDir = currentDir[0:len(currentDir)-1]
-    } else {
-      intFirstValue, err := strconv.Atoi(splitLine[0])
-      if err == nil {
-        for index, _ := range currentDir {
-          fullFilePath := strings.Join(currentDir[0:index+1], "/")
-          dirs[fullFilePath] += intFirstValue
-        }
-      }
-    }
-  }
   requiredSpace := 30000000 - (70000000 - dirs["/"])
   return findSmallestDirLargerThanCriteria(dirs, requiredSpace)
 }
